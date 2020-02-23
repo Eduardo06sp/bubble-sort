0a7b15a883ce163520d7d7a580a348ddf0ca9d47
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2020-02-23 13:30:41 -0800

    Reuse code from bubble_sort method, to be modified

--- sort.rb
+++ sort.rb
@@ -20,6 +20,28 @@ def bubble_sort(array)
   return array
 end
 
+def bubble_sort_by(array)
+  sorted = false
+
+  while !(sorted)
+    a = 0
+    b = 1
+    sorted = true
+
+    until b >= array.length
+      if array[a] > array[b]
+        sorted = false
+        array[a], array[b] = array[b], array[a]
+      end
+
+      a += 1
+      b += 1
+    end
+  end
+
+  return array
+end
+
 p bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
   left.length - right.length
 end
